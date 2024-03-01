74dbbd69e1b7c63c125498167f33e6027f19f715
Adrianna Kolosa <a.kolosa@infinity-group.pl>
2024-03-01 11:07:10 +0100

    Revert "my-json-server"
    This reverts commit cbc0c240a791a6f10271a7903a7693588f7eb784.

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -1,43 +1,41 @@
-import React, { useEffect, useState } from "react";
-import AddCommentForm from "./AddCommentForm";
-import CommentList from "./CommentList";
+import React, { useEffect, useState } from 'react'
+import AddCommentForm from './AddCommentForm'
+import CommentList from './CommentList'
 
 // Komponent CommentSection służy wyświetlania wszystkich komentarzy wraz z formularzem
 const CommentSection = ({ user }) => {
   // Stan komponentu: comments - aktualna lista komentarzy, error - ewentualny błąd przy pobieraniu komentarzy
-  const [comments, setComments] = useState([]);
-  const [error, setError] = useState("");
+  const [comments, setComments] = useState([])
+  const [error, setError] = useState('')
 
   // Funkcja asynchroniczna pobierająca komentarze z serwera
   const fetchComments = async () => {
     try {
-      // Pobieranie komentarzy z serwera
-      const response = await fetch(
-        "https://my-json-server.typicode.com/0adk/json-server-photogram"
-      );
+      // Pobieranie komentarzy z serwera 
+      const response = await fetch('/comments')
       // Obsługa odpowiedzi z serwera
       if (!response.ok) {
-        throw new Error("Nie można pobrać komentarzy");
+        throw new Error('Nie można pobrać komentarzy')
       }
       // Konwersja (parsowanie) odpowiedź serwera na listę komentarzy w formacie JSON
-      const data = await response.json();
+      const data = await response.json()
       // Aktualizacja stanu komentarzy
-      setComments(data);
+      setComments(data)
     } catch (error) {
       // Aktualizacja stanu błędu
-      setError(error.message);
+      setError(error.message)
     }
-  };
+  }
 
   // Wywołanie funkcji fetchComments() po pierwszym renderowaniu komponentu, aby pobrać komentarze z serwera
   useEffect(() => {
-    fetchComments();
-  }, []);
+    fetchComments()
+  }, [])
 
   // Wywołanie funkcji fetchComments() po dodaniu nowego komentarza, aby pobrać zaktualizowaną listę komentarzy o nowy komentarz
   const handleAddComment = () => {
-    fetchComments();
-  };
+    fetchComments()
+  }
 
   // Renderowanie komponentu CommentSection: wyświetlanie listy komentarzy lub błędu oraz formularza dodawania komentarza
   return (
@@ -49,7 +47,7 @@ const CommentSection = ({ user }) => {
       )}
       <AddCommentForm user={user} onAddComment={handleAddComment} />
     </div>
-  );
-};
+  )
+}
 
-export default CommentSection;
+export default CommentSection
